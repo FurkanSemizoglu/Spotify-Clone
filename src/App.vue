5dad79491c3b27e5adbbc3860bb13958d748e743
Furkan <furkan5590@hotmail.com>
2024-06-21 14:27:35 +0300

    project setup

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  sdfdsfds
+
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+</template>
