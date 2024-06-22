022dff751b3ea2c3a32a698a105cc876173b39e2
Furkan <furkan5590@hotmail.com>
2024-06-22 13:32:34 +0300

    colorfulCategory component was added

---
+++ src/components/ColorfulCategory.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import uniqolor from 'uniqolor'
+
+import { ref } from 'vue'
+interface ColorfulCategoryProps {
+  categoryName: string
+  image: string
+}
+
+const props = defineProps<ColorfulCategoryProps>()
+const randomColor = ref<any>('')
+
+randomColor.value = uniqolor.random()
+
+console.log(randomColor.value)
+</script>
+
+<template>
+  <div
+    :style="`background-color:${uniqolor.random().color}`"
+    class="contrast-[2] brightness-[0.87] rounded-xl h-[200px] relative overflow-hidden transition duration-500 ease-in-out hover:scale-105 cursor-pointer"
+  >
+    <div class="text-white pt-3 pl-4 font-semibold text-[23px]">{{ props.categoryName }}</div>
+    <img
+      class="absolute contrast-[0.55] -right-3.5 bottom-0 max-w-[100px] rotate-[20deg]"
+      :src="props.image || ''"
+    />
+  </div>
+</template>
--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,3 +1,41 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ColorfulCategory from '@/components/ColorfulCategory.vue'
+</script>
 
-<template>searchview</template>
+<template>
+  <div
+    class="text-white font-semibold hover:underline p-4 cursor-pointer inline-block text-2xl ml-4 mt-4"
+  >
+    Browse All
+  </div>
+
+  <div class="m-5"></div>
+
+  <div class="grid xl:grid-cols-4 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-6 px-5">
+    <ColorfulCategory categoryName="HipHop" image="https://picsum.photos/id/430/300/300" />
+    <ColorfulCategory categoryName="New Release" image="https://picsum.photos/id/56/300/300" />
+    <ColorfulCategory categoryName="New Release" image="https://picsum.photos/id/25/300/300" />
+    <ColorfulCategory categoryName="Podcasts" image="https://picsum.photos/id/103/300/300" />
+    <ColorfulCategory categoryName="Audiobooks" image="https://picsum.photos/id/244/300/300" />
+    <ColorfulCategory categoryName="Made For You" image="https://picsum.photos/id/202/300/300" />
+    <ColorfulCategory categoryName="New Release" image="https://picsum.photos/id/101/300/300" />
+    <ColorfulCategory categoryName="New Release" image="https://picsum.photos/id/130/300/300" />
+    <ColorfulCategory categoryName="Podcasts" image="https://picsum.photos/id/45/300/300" />
+    <ColorfulCategory categoryName="HipHop" image="https://picsum.photos/id/22/300/300" />
+    <ColorfulCategory categoryName="New Release" image="https://picsum.photos/id/26/300/300" />
+    <ColorfulCategory categoryName="New Release" image="https://picsum.photos/id/25/300/300" />
+    <ColorfulCategory categoryName="Podcasts" image="https://picsum.photos/id/153/300/300" />
+    <ColorfulCategory categoryName="Audiobooks" image="https://picsum.photos/id/244/300/300" />
+    <ColorfulCategory categoryName="Made For You" image="https://picsum.photos/id/202/300/300" />
+    <ColorfulCategory categoryName="New Release" image="https://picsum.photos/id/101/300/300" />
+    <ColorfulCategory categoryName="New Release" image="https://picsum.photos/id/120/300/300" />
+    <ColorfulCategory categoryName="Podcasts" image="https://picsum.photos/id/140/300/300" />
+    <ColorfulCategory categoryName="New Release" image="https://picsum.photos/id/120/300/300" />
+    <ColorfulCategory categoryName="Podcasts" image="https://picsum.photos/id/140/300/300" />
+    <ColorfulCategory categoryName="Made For You" image="https://picsum.photos/id/202/300/300" />
+    <ColorfulCategory categoryName="New Release" image="https://picsum.photos/id/101/300/300" />
+    <ColorfulCategory categoryName="New Release" image="https://picsum.photos/id/160/300/300" />
+    <ColorfulCategory categoryName="Podcasts" image="https://picsum.photos/id/140/300/300" />
+    <ColorfulCategory categoryName="New Release" image="https://picsum.photos/id/30/300/300" />
+  </div>
+</template>
