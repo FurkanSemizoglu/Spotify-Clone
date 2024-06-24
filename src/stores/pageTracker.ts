e53b73bd9d1784201d799e898b60ff472bc32c47
Furkan <furkan5590@hotmail.com>
2024-06-24 14:03:28 +0300

    pageTracker store was created

---
+++ src/stores/pageTracker.ts
@@ -0,0 +1,18 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+
+export const usePageTrackerStore = defineStore('pageTracker', () => {
+  const pages = ref<string[]>([])
+
+  function addPage(page: string) {
+    pages.value.push(page)
+  }
+
+  function getPage(index: number) {
+    return pages.value[index]
+  }
+
+  const pageCount = computed(() => pages.value.length)
+
+  return { pages, addPage, getPage, pageCount }
+})
